516d0b171a887d3e1f15a195784edd352ede14e3
Maschine2501 <57831222+Maschine2501@users.noreply.github.com>
2020-02-23 11:47:52 +0100

    Update oledui.py

--- oledui.py
+++ oledui.py
@@ -78,9 +78,9 @@ oled.clear()
 font = load_font('digi.ttf', 24)
 font2 = load_font('digi.ttf', 15)
 hugefontaw = load_font('fa-solid-900.ttf', oled.HEIGHT - 4)
-fontClock = load_font('digi.ttf', 50)
-fontDate = load_font('digi.ttf', 14)  
-fontIP = load_font('digi.ttf', 14)  
+fontClock = load_font('7seg.ttf', 50)
+fontDate = load_font('7seg.ttf', 14)  
+fontIP = load_font('7seg.ttf', 14)  
 #above are the "imports" for the fonts. 
 #After the name of the font comes a number, this defines the Size (height) of the letters. 
 #Just put .ttf file in the 'Volumio-OledUI/fonts' directory and make an import like above. 
