a2bb52af82e3cfeb5813d06017fb791ce7c7805f
Maschine2501 <57831222+Maschine2501@users.noreply.github.com>
2020-02-27 16:48:39 +0100

    Add files via upload
    Backup of stable release from 27.02.2020

--- oledui-last-stable.py
+++ oledui-last-stable.py
@@ -182,7 +182,7 @@ def onPushState(data):
         oled.volumeControlDisabled = data['disableVolumeControl']
 
     print(newSong.encode('ascii', 'ignore'))
-    if (newSong != oled.activeSong):    # new song
+    if (newSong != oled.activeSong) or (newArtist != oled.activeArtist):  # new song
         oled.activeSong = newSong
         oled.activeArtist = newArtist
         if oled.state == STATE_PLAYER and newStatus != 'stop':
@@ -530,7 +530,8 @@ else:
     
 if oled.playState != 'play':
     volumioIO.emit('play', {'value':oled.playPosition})
-varcanc = True
+varcanc = True #helper for pause -> stop timeout counter
+InfoTag = 0    #helper for missing Artist/Song when changing sources
 while True:
     if emit_volume:
         emit_volume = False
@@ -544,9 +545,15 @@ while True:
             pass
         volumioIO.emit('play', {'value':oled.playPosition})
     sleep(0.1)
+#this is the loop to get artist/song when changing sources (loops three times)
+    if oled.state == STATE_PLAYER and InfoTag <= 3 and newStatus != 'stop':
+        oled.modal.UpdatePlayingInfo(oled.activeArtist, oled.activeSong)
+        InfoTag += 1
+        sleep(1.5)
     #this is the loop to push the actual time every 0.1sec to the "Standby-Screen"
     if oled.state == STATE_PLAYER and newStatus == 'stop':  
-   	oled.time = strftime("%H:%M:%S")
+   	InfoTag = 0  #resets the InfoTag helper from artist/song update loop
+	oled.time = strftime("%H:%M:%S")
    	oled.modal.UpdateStandbyInfo(oled.time, oled.IP, oled.date)
     #if playback is paused, here is defined when the Player goes back to "Standby"/Stop	
     if oled.state == STATE_PLAYER and newStatus == 'pause' and varcanc == True:
