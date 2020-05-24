84419af82fb24ba1ac9fe6ea0dffbb95529b8647
Maschine2501 <57831222+Maschine2501@users.noreply.github.com>
2020-05-24 06:54:29 +0200

    Create Standby.py

---
+++ Standby.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from subprocess import call
+import RPi.GPIO as GPIO
+from time import sleep
+
+Standby = 37
+
+GPIO.setmode(GPIO.BOARD) # Set pin numbering to board numbering
+
+GPIO.setup(Standby, GPIO.IN) # Set up PinSeven as an input
+
+while (GPIO.input(Standby) == GPIO.LOW: # While button not pressed
+    GPIO.wait_for_edge(Standby, GPIO.RISING) # Wait for a rising edge on PinSeven
+    sleep(0.2); # Sleep 100ms to avoid triggering a shutdown when a spike occured
+    if (GPIO.input(Standby) == GPIO.HIGH:
+      call('poweroff', shell = False) # Initiate OS Poweroff
