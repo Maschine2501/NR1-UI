364f2b82c853f929dbc8f1eef5de8e3f2f43c237
Maschine2501 <57831222+Maschine2501@users.noreply.github.com>
2020-06-06 20:37:30 +0200

    Update StatusLED.py

--- modules/StatusLED.py
+++ modules/StatusLED.py
@@ -1,70 +1,106 @@
 #!/usr/bin//python3
-import psutil
-from time import sleep
-from gpiozero import LED
 
-CPU0 = LED(13)
-CPU3 = LED(12)
-CPU6 = LED(15)
-PlayLED = LED(16)
-StereoLED = LED(14)
+import sys, tty, termios, os, readchar
+from time import sleep
+import psutil
+from pcf8574 import PCF8574
+
+i2c_port_num = 1
+pcf_address = 0x20
+pcf = PCF8574(i2c_port_num, pcf_address)
 
 def SysStart():
-    CPU0.off()
-    CPU3.off()
-    CPU6.off()
-    PlayLED.off()
-    StereoLED.off()
+    pcf.port[7] = True
+    pcf.port[6] = True
+    pcf.port[5] = True
+    pcf.port[4] = True
+    pcf.port[3] = True
+    pcf.port[2] = True
+    pcf.port[1] = True
+    pcf.port[0] = True
     sleep(0.1)
-    CPU0.on()
-    PlayLED.on()
+    pcf.port[2] = False
     sleep(0.5)
-    CPU3.on()
-    StereoLED.on()
+    pcf.port[7] = False
     sleep(0.5)
-    CPU6.on()
-    sleep(1.0)
-    CPU6.off()
+    pcf.port[6] = False
     sleep(0.5)
-    CPU6.off()
-    StereoLED.off()
+    pcf.port[5] = False
     sleep(0.5)
-    CPU0.off()
-    PlayLED.off()
-    
-def ProcessorLED():
-    while True:
-        picpu = int(psutil.cpu_percent(percpu=False))
-        sleep(0.5)
-        if picpu < 5:
-            CPU0.off()
-            CPU3.off()
-            CPU6.off()
-            sleep(0.5)
-        if picpu < 33 and picpu > 5:
-            CPU0.on()
-            CPU3.off()
-            CPU6.off()
-            sleep(0.5)
-        if picpu > 33 or picpu < 66:
-            CPU0.on()
-            CPU3.on()
-            CPU6.off()
-            sleep(0.5)
-        if picpu > 66:
-            CPU0.on()
-            CPU3.on()
-            CPU6.on()
-            sleep(0.5)
-    
+    pcf.port[4] = False
+    sleep(0.5)
+    pcf.port[3] = False
+    sleep(0.5)
+    pcf.port[1] = False
+    sleep(0.5)
+    pcf.port[0] = False
+    sleep(0.5)
+    pcf.port[3] = True
+    sleep(0.5)
+    pcf.port[4] = True
+    sleep(0.5)
+    pcf.port[5] = True
+    sleep(0.5)
+    pcf.port[6] = True
+    pcf.port[0] = True
+    sleep(0.5)
+    pcf.port[7] = True
+    pcf.port[1] = True
+    sleep(0.5)
+
+def CPUload():
+   while True:
+       CPUpercent = psutil.cpu_percent(interval=0.2, percpu=False)
+       pcf.port[7] = True
+       pcf.port[6] = True
+       pcf.port[5] = True
+       pcf.port[4] = True
+       pcf.port[3] = True
+       if CPUpercent > 75:
+         pcf.port[7] = False
+         pcf.port[6] = False
+         pcf.port[5] = False
+         pcf.port[4] = False
+         pcf.port[3] = False
+       elif CPUpercent > 50 :
+         pcf.port[7] = False
+         pcf.port[6] = False
+         pcf.port[5] = False
+         pcf.port[4] = False
+         pcf.port[3] = True
+       elif CPUpercent > 25:
+         pcf.port[7] = False  
+         pcf.port[6] = False
+         pcf.port[5] = False
+         pcf.port[4] = True
+         pcf.port[3] = True
+       elif CPUpercent > 10:
+         pcf.port[7] = False
+         pcf.port[6] = False
+         pcf.port[5] = True
+         pcf.port[4] = True
+         pcf.port[3] = True
+       elif CPUpercent > 2:
+         pcf.port[7] = False
+         pcf.port[6] = True
+         pcf.port[5] = True
+         pcf.port[4] = True
+         pcf.port[3] = True
+       elif CPUpercent < 2:
+         pcf.port[7] = True
+         pcf.port[6] = True
+         pcf.port[5] = True
+         pcf.port[4] = True
+         pcf.port[3] = True
+
 def PlayLEDon():
-    PlayLED.on()
+    pcf.port[1] = False
     
 def StereoLEDon():
-    StereoLED.on()
+    pcf.port[1] = False
     
 def PlayLEDoff():
-    PlayLED.off()
+    pcf.port[1] = True
     
 def StereoLEDoff():
-    StereoLED.off()
+    pcf.port[1] = True
